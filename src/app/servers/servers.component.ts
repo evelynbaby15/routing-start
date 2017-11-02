0c926b2f3283b6407237fb899a479b6695900819
evelynbaby15 <evelyn.b9044015@gmail.com>
2017-11-02 16:29:22 +0800

    Resolve conflict

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -19,7 +19,10 @@ export class ServersComponent implements OnInit {
   }
 
   onReload() {
-    this.router.navigate(['servers'], {relativeTo: this.route});
+    // Note router.navigate 跟 routerLink 不同，routerLink 會按照在 app module 中定義的 route map 取得相對應的 path, 但是
+    // navigate 則是可單傳一個參數，如果要找出相對的路徑對應基準需要再給第二個 relativeTo 參數物件
+    // 但在這邊使用的話會錯，因為下面這行會形成 http://localhost:4200/servers/servers 的 URL
+    // this.router.navigate(['servers'], {relativeTo: this.route});
   }
 
 }
